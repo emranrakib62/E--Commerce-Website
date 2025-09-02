b2a0c88b94f874d6b2049471176d10e0d64ebb2c
Emranul Hoque <emranrakib01862@gmail.com>
2025-09-02 14:45:22 +0600

    updated

--- image/java.js
+++ image/java.js
@@ -3,3 +3,11 @@ Window.addEventListener("scroll",function(){
 header.classList.toggle("sticky",this.window.scrollY>0);
 })
 
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navmenu.classList.toggle('open');
+}
+
